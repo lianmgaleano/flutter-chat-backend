4890f7d5a25c621613523fcaedf604653c7c694c
Libardo Morales <lianmgaleano@gmail.com>
2023-04-14 09:41:01 -0500

    Fin chat

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -20,6 +20,21 @@ const generarJWT = (uid) => {
 
 }
 
+const comprobarJWT = (token = '') => {
+
+    try {
+
+        const {uid} = jwt.verify(token, process.env.JWT_KEY);
+
+        return [true, uid];
+
+    } catch(error) {
+        return [false];
+    }
+
+}
+
 module.exports = {
-    generarJWT
+    generarJWT,
+    comprobarJWT
 }
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,32 @@
-const { io } = require('../index');
-
+const {io} = require('../index');
+const {comprobarJWT} = require('../helpers/jwt');
+const {usuarioConectado, usuarioDesconectado, grabarMensaje} = require('../controllers/socket');
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', async (client) => {
     console.log('Cliente conectado');
 
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    //Verificar autenticacion
+    if (!valido) { return client.disconnect();}
+
+    //Cliente autenticado
+    await usuarioConectado(uid);
+
+    //Ingresar al usuario a una sala en aprticular
+    // sala global, client.id 6437ea8b7b97665f5657f7f5
+    client.join(uid);
+
+    //Escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', async (payload) => {
+        await grabarMensaje(payload);
+
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
+
+    client.on('disconnect', async () => {
+        await usuarioDesconectado(uid);
     });
 
 });
